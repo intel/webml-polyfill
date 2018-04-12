7a2466b568936098da7324d80e24ecd654c8ec61
ganwenyao <wenyao.gan@intel.com>
2018-04-12 19:06:18 +0800

    support SqueezeNet for webgl2

--- src/nn/webgl2/Model.js
+++ src/nn/webgl2/Model.js
@@ -1,5 +1,6 @@
 import {OperationCodeToLayersMap, OperationCodeAttrsMap, WebGL2SpecialLayers} from './utils/modelUtils'
 import Tensor from './Tensor'
+import ops from 'ndarray-ops'
 
 /**
  * WebGL2 Model class
@@ -12,7 +13,7 @@ export default class Model {
    */
   constructor(model) {
     this._model = model;
-    this._operands = model._operands;
+    this._operands = Array(model._operands.length);
     this._layers = [];
     this.supportTopClasses = false; 
 
@@ -26,7 +27,8 @@ export default class Model {
     return new Promise((resolve) => {
       this._layers.push(new WebGL2SpecialLayers.Input());
       this._model._operations.forEach(op => {
-        let attrs = OperationCodeAttrsMap.get(op.type)(this._operands, op.inputs, op.output);
+        // console.log(op);
+        let attrs = OperationCodeAttrsMap.get(op.type)(this._model._operands, op.inputs, op.outputs);
         let LayerClass = OperationCodeToLayersMap.get(op.type);
         this._layers.push(new LayerClass(attrs));
       });
@@ -46,25 +48,40 @@ export default class Model {
    */
   execute(inputs, outputs) {
     return new Promise((resolve) => {
-      let nnOperands = this._operands;      
+      let nnOperands = this._model._operands;
+      let nnOperations = this._model._operations
       let inputBuffer = inputs[0].buffer;
       let inputIndex = inputs[0].index;
+      let outputBuffer = outputs[0].buffer;
+      let outputIndex = outputs[0].index;
+
       let inputTensor = new Tensor(inputBuffer, nnOperands[inputIndex].dimensions.slice(1,4));
+
       let TopClassesOut;
       let output;
       this._layers.forEach((layer, i) => {
         if (i == 0) {
-          output = layer.call(inputBuffer, nnOperands[inputIndex].dimensions.slice(1,4), Float32Array);
+          this._operands[inputIndex] = layer.call(inputBuffer, nnOperands[inputIndex].dimensions.slice(1,4), Float32Array);
+          this._operands[inputIndex].transferFromGLTexture();
         } else if (this.supportTopClasses && i === this._layers.length - 1){
-          output = layer.call(output);
+          this._operands[layer.outputs[0]] = layer.call(this._operands[layer.inputs[0]]);
         } else {
-          output = layer.call(output);
+          if (layer.inputs.length === 1) {
+            this._operands[layer.outputs[0]] = layer.call(this._operands[layer.inputs[0]]);
+          } else {
+            let MutiInputs = [];
+            layer.inputs.forEach(input => {
+              MutiInputs.push(this._operands[input]);
+            });
+            this._operands[layer.outputs[0]] = layer.call(MutiInputs);
+          }
+          // this._operands[layer.outputs[0]].transferFromGLTexture();
         }
-        // console.log(layer)
+        // console.log(i, layer)
       });
-      output.transferFromGLTexture();
-      let outputBuffer = outputs[0].buffer;
-      outputBuffer.set(output.tensor.data);
+      this._operands[outputIndex].transferFromGLTexture();
+      // console.log(outputIndex, this._operands[outputIndex])
+      outputBuffer.set(this._operands[outputIndex].tensor.data);
       resolve('execute success');
     });
   }
