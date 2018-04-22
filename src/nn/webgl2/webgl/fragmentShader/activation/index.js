730b808e6615d0c42eb6919c6e64ecbf4d7dcf04
ganwenyao <807105461@qq.com>
2018-04-22 09:46:45 +0800

    [WebGL2] fuse avtivation

--- src/nn/webgl2/webgl/fragmentShader/activation/index.js
+++ src/nn/webgl2/webgl/fragmentShader/activation/index.js
@@ -2,10 +2,12 @@ import { RELU } from './RELU'
 import { RELU1 } from './RELU1'
 import { RELU6 } from './RELU6'
 import { softmax } from './softmax'
+import { fuse } from './fuse'
 
 export { 
   RELU, 
   RELU1, 
-  RELU6, 
-  softmax 
+  RELU6,
+  fuse,
+  softmax
 }
--- src/nn/webgl2/webgl/fragmentShader/arithmetic/matMul.js
+++ src/nn/webgl2/webgl/fragmentShader/arithmetic/matMul.js
@@ -1,32 +1,35 @@
-export const matMulShaderSource = `#version 300 es
-precision highp int;
-precision highp float;
-precision highp sampler2D;
-
-in vec2 outTex;
-uniform sampler2D A;
-uniform sampler2D B;
-uniform sampler2D C;
-uniform bool addC;
-out vec4 outColor;
-
-void main() {
-  ivec2 A_size = textureSize(A, 0);
-  ivec2 B_size = textureSize(B, 0);
-  int out_x = int(float(B_size[0]) * outTex.x);
-  int out_y = int(float(A_size[1]) * outTex.y);
-  int commonDim = A_size[0];
-
-  float sum = 0.0;
-  for (int i = 0; i < commonDim; ++i) {
-    float a = texelFetch(A, ivec2(i, out_y), 0).r;
-    float b = texelFetch(B, ivec2(out_x, i), 0).r;
-    sum += a * b;
-  }
-
-  if (addC) {
-    sum += texelFetch(C, ivec2(out_x, 0), 0).r;
-  }
-
-  outColor = vec4(sum);
-}`;
+export default function matMul(fuse) {
+  const source = `#version 300 es
+  precision highp float;
+  precision highp sampler2D;
+  
+  in vec2 outTex;
+  uniform sampler2D A;
+  uniform sampler2D B;
+  uniform sampler2D C;
+  uniform bool addC;
+  out vec4 outColor;
+  
+  void main() {
+    ivec2 A_size = textureSize(A, 0);
+    ivec2 B_size = textureSize(B, 0);
+    int out_x = int(float(B_size[0]) * outTex.x);
+    int out_y = int(float(A_size[1]) * outTex.y);
+    int commonDim = A_size[0];
+  
+    float sum = 0.0;
+    for (int i = 0; i < commonDim; ++i) {
+      float a = texelFetch(A, ivec2(i, out_y), 0).r;
+      float b = texelFetch(B, ivec2(out_x, i), 0).r;
+      sum += a * b;
+    }
+  
+    if (addC) {
+      sum += texelFetch(C, ivec2(out_x, 0), 0).r;
+    }
+  
+    ${fuse}
+    outColor = vec4(sum);
+  }`;
+  return source;
+}
