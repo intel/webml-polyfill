6287a04d323a8f3729645b5571d278123163e03f
ganwenyao <wenyao.gan@intel.com>
2018-04-24 14:11:07 +0800

    [WebGL2] read SSD_MobileNet model

---
+++ examples/ssd_mobilenet/camera.js
@@ -0,0 +1,78 @@
+function main() {
+  let utils = new Utils();
+  const videoElement = document.getElementById('video');
+  let streaming = false;
+  const backend = document.getElementById('backend');
+  const wasm = document.getElementById('wasm');
+  const webgl = document.getElementById('webgl');
+  const webml = document.getElementById('webml');
+  let currentBackend = '';
+
+  function updateBackend() {
+    currentBackend = utils.model._backend;
+    if (getUrlParams('api_info') === 'true') {
+      backend.innerHTML = currentBackend === 'WebML' ? currentBackend + '/' + getNativeAPI() : currentBackend;
+    } else {
+      backend.innerHTML = currentBackend;
+    }
+  }
+
+  function changeBackend(newBackend) {
+    if (currentBackend === newBackend) {
+      return;
+    }
+    backend.innerHTML = 'Setting...';
+    setTimeout(() => {
+      utils.init(newBackend).then(() => {
+        updateBackend();
+      });
+    }, 10);
+  }
+ 
+  if (nnNative) {
+    webml.setAttribute('class', 'dropdown-item');
+    webml.onclick = function (e) {
+      changeBackend('WebML');
+    }
+  }
+
+  if (nnPolyfill.supportWebGL2) {
+    webgl.setAttribute('class', 'dropdown-item');
+    webgl.onclick = function(e) {
+      changeBackend('WebGL2');
+    }
+  }
+
+  if (nnPolyfill.supportWasm) {
+    wasm.setAttribute('class', 'dropdown-item');
+    wasm.onclick = function(e) {
+      changeBackend('WASM');
+    }
+  }
+
+  let stats = new Stats();
+  stats.dom.style.cssText = 'position:fixed;top:60px;left:10px;cursor:pointer;opacity:0.9;z-index:10000';
+  stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+  document.body.appendChild(stats.dom);
+
+  navigator.mediaDevices.getUserMedia({audio: false, video: {facingMode: "environment"}}).then((stream) => {
+    video.srcObject = stream;
+    utils.init().then(() => {
+      updateBackend();
+      streaming = true;
+      startPredict();
+    });
+  }).catch((error) => {
+    console.log('getUserMedia error: ' + error.name, error);
+  });
+
+  function startPredict() {
+    stats.begin();
+    utils.predict(videoElement).then(() => {
+      stats.end();
+      if (streaming) {
+        setTimeout(startPredict, 0);
+      }
+    });
+  }
+}
---
+++ examples/ssd_mobilenet/utils.js
@@ -0,0 +1,199 @@
+const INPUT_TENSOR_SIZE = 300*300*3;
+const OUTPUT_TENSOR_SIZE = (1083 + 600 + 150 + 54 + 24 + 6) * (4 + 91);
+const MODEL_FILE = './model/ssd_mobilenet.tflite';
+const LABELS_FILE = './model/coco_labels_list.txt';
+
+class Utils {
+  constructor() {
+    this.tfModel;
+    this.labels;
+    this.model;
+    this.inputTensor;
+    this.outputTensor;
+
+    this.inputTensor = new Float32Array(INPUT_TENSOR_SIZE);
+    this.outputTensor = new Float32Array(OUTPUT_TENSOR_SIZE);
+    this.container = document.getElementById('container');
+    this.progressBar = document.getElementById('progressBar');
+    this.progressContainer = document.getElementById('progressContainer');
+    this.canvasElement = document.getElementById('canvas');
+    this.canvasContext = this.canvasElement.getContext('2d');
+
+    this.initialized = false;
+  }
+
+  async init(backend) {
+    this.initialized = false;
+    let result;
+    if (!this.tfModel) {
+      result = await this.loadModelAndLabels(MODEL_FILE, LABELS_FILE);
+      this.container.removeChild(progressContainer);
+      this.labels = result.text.split('\n');
+      console.log(`labels: ${this.labels}`);
+      let flatBuffer = new flatbuffers.ByteBuffer(result.bytes);
+      this.tfModel = tflite.Model.getRootAsModel(flatBuffer);
+      printTfLiteModel(this.tfModel);
+    }
+    this.model = new MobileNet(this.tfModel, backend);
+    result = await this.model.createCompiledModel();
+    console.log(`compilation result: ${result}`);
+    let start = performance.now();
+    result = await this.model.compute(this.inputTensor, this.outputTensor);
+    let elapsed = performance.now() - start;
+    console.log(`warmup time: ${elapsed.toFixed(2)} ms`);
+    console.log(this.outputTensor)
+    // this.initialized = true;
+  }
+
+  async predict(imageSource) {
+    if (!this.initialized) return;
+    this.canvasContext.drawImage(imageSource, 0, 0,
+                                 this.canvasElement.width,
+                                 this.canvasElement.height);
+    this.prepareInputTensor(this.inputTensor, this.canvasElement);
+    let start = performance.now();
+    let result = await this.model.compute(this.inputTensor, this.outputTensor);
+    let elapsed = performance.now() - start;
+    let classes = this.getTopClasses(this.outputTensor, this.labels, 3);
+    console.log(`Inference time: ${elapsed.toFixed(2)} ms`);
+    let inferenceTimeElement = document.getElementById('inferenceTime');
+    inferenceTimeElement.innerHTML = `inference time: ${elapsed.toFixed(2)} ms`;
+    console.log(`Classes: `);
+    classes.forEach((c, i) => {
+      console.log(`\tlabel: ${c.label}, probability: ${c.prob}%`);
+      let labelElement = document.getElementById(`label${i}`);
+      let probElement = document.getElementById(`prob${i}`);
+      labelElement.innerHTML = `${c.label}`;
+      probElement.innerHTML = `${c.prob}%`;
+    });
+  }
+
+  async loadModelAndLabels(modelUrl, labelsUrl) {
+    let arrayBuffer = await this.loadUrl(modelUrl, true, true);
+    let bytes = new Uint8Array(arrayBuffer);
+    let text = await this.loadUrl(labelsUrl);
+    return {bytes: bytes, text: text};
+  }
+
+  async loadUrl(url, binary, progress) {
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      request.open('GET', url, true);
+      if (binary) {
+        request.responseType = 'arraybuffer';
+      }
+      request.onload = function(ev) {
+        if (request.readyState === 4) {
+          if (request.status === 200) {
+              resolve(request.response);
+          } else {
+              reject(new Error('Failed to load ' + modelUrl + ' status: ' + request.status));
+          }
+        }
+      };
+      if (progress) {
+        let self = this;
+        request.onprogress = function(ev) {
+          if (ev.lengthComputable) {
+            let percentComplete = ev.loaded / ev.total * 100;
+            percentComplete = percentComplete.toFixed(0);
+            self.progressBar.style = `width: ${percentComplete}%`;
+            self.progressBar.innerHTML = `${percentComplete}%`;
+          }
+        }
+      }
+      request.send();
+    });
+  }
+  
+  prepareInputTensor(tensor, canvas) {
+    const width = 224;
+    const height = 224;
+    const channels = 3;
+    const imageChannels = 4; // RGBA
+    const mean = 127.5;
+    const std = 127.5;
+    if (canvas.width !== width || canvas.height !== height) {
+      throw new Error(`canvas.width(${canvas.width}) or canvas.height(${canvas.height}) is not 224`);
+    }
+    let context = canvas.getContext('2d');
+    let pixels = context.getImageData(0, 0, width, height).data;
+    // NHWC layout
+    for (let y = 0; y < height; ++y) {
+      for (let x = 0; x < width; ++x) {
+        for (let c = 0; c < channels; ++c) {
+          let value = pixels[y*width*imageChannels + x*imageChannels + c];
+          tensor[y*width*channels + x*channels + c] = (value - mean)/std;
+        }
+      }
+    }
+  }
+  
+  getTopClasses(tensor, labels, k = 5) {
+    let probs = Array.from(tensor);
+    let indexes = probs.map((prob, index) => [prob, index]);
+    let sorted = indexes.sort((a, b) => {
+      if (a[0] === b[0]) {return 0;}
+      return a[0] < b[0] ? -1 : 1;
+    });
+    sorted.reverse();
+    let classes = [];
+    for (let i = 0; i < k; ++i) {
+      let prob = sorted[i][0];
+      let index = sorted[i][1];
+      let c = {
+        label: labels[index],
+        prob: (prob * 100).toFixed(2)
+      }
+      classes.push(c);
+    }
+    return classes;
+  }
+}
+
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+      platform = window.navigator.platform,
+      macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+      windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+      iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+      os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (!os && /Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
+
+function getNativeAPI() {
+  const apiMapping = {
+    'Mac OS': 'MPS',
+    'Android': 'NN',
+    'Windows': 'DirectML',
+    'Linux': 'N/A'
+  };
+
+  return apiMapping[getOS()];
+}
+
+function getUrlParams( prop ) {
+  var params = {};
+  var search = decodeURIComponent( window.location.href.slice( window.location.href.indexOf( '?' ) + 1 ) );
+  var definitions = search.split( '&' );
+
+  definitions.forEach( function( val, key ) {
+    var parts = val.split( '=', 2 );
+      params[ parts[ 0 ] ] = parts[ 1 ];
+  } );
+
+  return ( prop && prop in params ) ? params[ prop ] : params;
+}
