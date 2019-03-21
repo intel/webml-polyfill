8bb0dcc59002f496eaeb03d420c6b22a5de08fc5
cuiyan <yanx.cui@intel.com>
2019-03-21 15:25:09 +0800

    [test] Update test case names and move 2 nn-test-cases to plus
    Signed-off-by: cuiyan <yanx.cui@intel.com>

--- test/cts/test/cts-all.js
+++ test/cts/test/cts-all.js
@@ -7488,102 +7488,7 @@ describe('CTS', function() {
     }
   });
 
-  it('check result for Reshape example', async function() {
-    let model = await nn.createModel(options);
-    let operandIndex = 0;
-
-    let op1_value = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let op3_expect = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    let type0 = {type: nn.TENSOR_FLOAT32, dimensions: [1, 1, 3, 3]};
-    let type0_length = product(type0.dimensions);
-    let type2 = {type: nn.TENSOR_FLOAT32, dimensions: [9]};
-    let type2_length = product(type2.dimensions);
-    let type1 = {type: nn.TENSOR_INT32, dimensions: [1]};
-    let type1_length = product(type1.dimensions);
-
-    let op1 = operandIndex++;
-    model.addOperand(type0);
-    let op2 = operandIndex++;
-    model.addOperand(type1);
-    let op3 = operandIndex++;
-    model.addOperand(type2);
-
-    model.setOperandValue(op2, new Int32Array([-1]));
-    model.addOperation(nn.RESHAPE, [op1, op2], [op3]);
-
-    model.identifyInputsAndOutputs([op1], [op3]);
-    await model.finish();
-
-    let compilation = await model.createCompilation();
-    compilation.setPreference(getPreferenceCode(options.prefer));
-    await compilation.finish();
-
-    let execution = await compilation.createExecution();
-
-    let op1_input = new Float32Array(op1_value);
-    execution.setInput(0, op1_input);
-
-    let op3_output = new Float32Array(type2_length);
-    execution.setOutput(0, op3_output);
-
-    await execution.startCompute();
-
-    for (let i = 0; i < type2_length; ++i) {
-      assert.isTrue(almostEqualCTS(op3_output[i], op3_expect[i]));
-    }
-  });
-
-  it('check result for Reshape weights as inputs example', async function() {
-    let model = await nn.createModel(options);
-    let operandIndex = 0;
-
-    let op1_value = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let op2_value = [-1];
-    let op3_expect = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-    let type0 = {type: nn.TENSOR_FLOAT32, dimensions: [1, 1, 3, 3]};
-    let type0_length = product(type0.dimensions);
-    let type2 = {type: nn.TENSOR_FLOAT32, dimensions: [9]};
-    let type2_length = product(type2.dimensions);
-    let type1 = {type: nn.TENSOR_INT32, dimensions: [1]};
-    let type1_length = product(type1.dimensions);
-
-    let op1 = operandIndex++;
-    model.addOperand(type0);
-    let op2 = operandIndex++;
-    model.addOperand(type1);
-    let op3 = operandIndex++;
-    model.addOperand(type2);
-
-    let op2_input = new Int32Array(op2_value);
-    model.setOperandValue(op2, op2_input);
-
-    model.addOperation(nn.RESHAPE, [op1, op2], [op3]);
-
-    model.identifyInputsAndOutputs([op1], [op3]);
-    await model.finish();
-
-    let compilation = await model.createCompilation();
-    compilation.setPreference(getPreferenceCode(options.prefer));
-    await compilation.finish();
-
-    let execution = await compilation.createExecution();
-
-    let op1_input = new Float32Array(op1_value);
-    execution.setInput(0, op1_input);
-
-    let op3_output = new Float32Array(type2_length);
-    execution.setOutput(0, op3_output);
-
-    await execution.startCompute();
-
-    for (let i = 0; i < type2_length; ++i) {
-      assert.isTrue(almostEqualCTS(op3_output[i], op3_expect[i]));
-    }
-  });
-
-  it('check result for Resize bilinear example', async function() {
+  it('check result for Resize bilinear with inputs (without align_corners) example', async function() {
     let model = await nn.createModel(options);
     let operandIndex = 0;
 
@@ -7631,7 +7536,7 @@ describe('CTS', function() {
     }
   });
 
-  it('check result for Resize bilinear example/2', async function() {
+  it('check result for Resize bilinear with inputs (without align_corners) example/2', async function() {
     let model = await nn.createModel(options);
     let operandIndex = 0;
 
