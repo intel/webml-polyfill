4611e09674debb3c97db722b464f3f16f4709d9c
cuiyan <yanx.cui@intel.com>
2019-10-22 09:59:18 +0800

    [test] Add CTS test cases for LOGISTIC op
    Signed-off-by: cuiyan <yanx.cui@intel.com>

---
+++ test/cts/test/V1_0/logistic_quant8_1.js
@@ -0,0 +1,45 @@
+describe('CTS', function() {
+  const assert = chai.assert;
+  const nn = navigator.ml.getNeuralNetworkContext();
+
+  it('check result for Logistic quant8 example/1', async function() {
+    let model = await nn.createModel(options);
+    let operandIndex = 0;
+
+    let op1_value = [0, 1, 2, 127];
+    let op3_expect = [128, 159, 187, 255];
+
+    let type1 = {type: nn.TENSOR_QUANT8_ASYMM, dimensions: [1, 2, 2, 1], scale: 0.00390625, zeroPoint: 0};
+    let type1_length = product(type1.dimensions);
+    let type0 = {type: nn.TENSOR_QUANT8_ASYMM, dimensions: [1, 2, 2, 1], scale: 0.5, zeroPoint: 0};
+    let type0_length = product(type0.dimensions);
+
+    let op1 = operandIndex++;
+    model.addOperand(type0);
+    let op3 = operandIndex++;
+    model.addOperand(type1);
+
+    model.addOperation(nn.LOGISTIC, [op1], [op3]);
+
+    model.identifyInputsAndOutputs([op1], [op3]);
+    await model.finish();
+
+    let compilation = await model.createCompilation();
+    compilation.setPreference(getPreferenceCode(options.prefer));
+    await compilation.finish();
+
+    let execution = await compilation.createExecution();
+
+    let op1_input = new Uint8Array(op1_value);
+    execution.setInput(0, op1_input);
+
+    let op3_output = new Uint8Array(type1_length);
+    execution.setOutput(0, op3_output);
+
+    await execution.startCompute();
+
+    for (let i = 0; i < type1_length; ++i) {
+      assert.isTrue(almostEqualCTS(op3_output[i], op3_expect[i]));
+    }
+  });
+});
