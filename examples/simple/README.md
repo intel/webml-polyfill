a1ee983ddee1ef912c9847ed00586e1a77e2dcb8
Ningxin Hu <ningxin.hu@intel.com>
2020-06-29 11:28:51 +0800

    Use string keys in simple example

--- examples/simple/README.md
+++ examples/simple/README.md
@@ -1,30 +1,27 @@
 WebNN API Sample
 ======
-This sample is a mimic of [Android Neural Networks API Sample](https://github.com/googlesamples/android-ndk/tree/master/nn_sample).
+This sample is based on the [WebNN examples](https://webmachinelearning.github.io/webnn/#examples).
 
 It demonstrates basic usages of WebNN API with a simple model that consists of three operations: two additions and a multiplication.
 
-The sums created by the additions are the inputs to the multiplication. In essence, we are creating a graph that computes: (tensor0 + tensor1) * (tensor2 + tensor3).
+The sums created by the additions are the inputs to the multiplication. In essence, we are creating a graph that computes: (constant1 + input1) * (constant2 + input2).
 ```js
-tensor0 ---+
-           +--- ADD ---> intermediateOutput0 ---+
-tensor1 ---+                                    |
-                                                +--- MUL---> output
-tensor2 ---+                                    |
-           +--- ADD ---> intermediateOutput1 ---+
-tensor3 ---+
+constant1 ---+
+             +--- ADD ---> intermediateOutput0 ---+
+input1    ---+                                    |
+                                                  +--- MUL---> output
+constant2 ---+                                    |
+             +--- ADD ---> intermediateOutput1 ---+
+input2    ---+
 ```
 
-Two of the four tensors, tensor0 and tensor2 being added are constants, defined in the model. They represent the weights that would have been learned during a training process, loaded from model_data.bin.
+Two of the four tensors, constant1 and constant2 being added are constants, defined in the model. They represent the weights that would have been learned during a training process, loaded from model_data.bin.
 
-The other two tensors, tensor1 and tensor3 will be inputs to the model. Their values will be provided when we execute the model. These values can change from execution to execution.
+The other two tensors, input1 and input2 will be inputs to the model. Their values will be provided when we execute the model. These values can change from execution to execution.
 
-Besides the two input tensors, an optional fused activation function can also be defined for ADD and MUL. In this example, we'll simply set it to NONE.
-
-The model then has 8 operands:
+The model then has 7 operands:
 - 2 tensors that are inputs to the model. These are fed to the two ADD operations.
 - 2 constant tensors that are the other two inputs to the ADD operations.
-- 1 fuse activation operand reused for the ADD operations and the MUL operation.
 - 2 intermediate tensors, representing outputs of the ADD operations and inputs to the MUL operation.
 - 1 model output.
 
--- examples/simple/SimpleModel.js
+++ examples/simple/SimpleModel.js
@@ -16,34 +16,34 @@ class SimpleModel {
     // Create OperandDescriptor object.
     const float32TensorType = {type: 'tensor-float32', dimensions: [this.tensorSize_]};
 
-    // tensor0 is a constant tensor. Set its value from an ArrayBuffer object.
+    // constant1 is a constant tensor. Set its value from an ArrayBuffer object.
     // The ArrayBuffer object may contain the training data loaded before hand.
-    const tensor0 = nn.constant(float32TensorType,
-                                new Float32Array(arrayBuffer, 0, this.tensorSize_));
+    const constant1 = nn.constant(float32TensorType,
+                                  new Float32Array(arrayBuffer, 0, this.tensorSize_));
 
-    // tensor1 is one of the input tensors. Its value will be set before execution.
-    const tensor1 = nn.input(float32TensorType);
+    // input1 is one of the input tensors. Its value will be set before execution.
+    const input1 = nn.input('input1', float32TensorType);
 
-    // tensor2 is another constant tensor. Set its value from same ArrayBuffer
+    // constant2 is another constant tensor. Set its value from same ArrayBuffer
     // object with offset.
-    const tensor2 = nn.constant(float32TensorType,
-                                new Float32Array(arrayBuffer,
-                                                 this.tensorSize_ * Float32Array.BYTES_PER_ELEMENT,
-                                                 this.tensorSize_));
+    const constant2 = nn.constant(float32TensorType,
+                                  new Float32Array(arrayBuffer,
+                                                   this.tensorSize_ * Float32Array.BYTES_PER_ELEMENT,
+                                                   this.tensorSize_));
 
-    // tensor3 is another input tensor. Its value will be set before execution.
-    const tensor3 = nn.input(float32TensorType);
+    // input2 is another input tensor. Its value will be set before execution.
+    const input2 = nn.input('input2', float32TensorType);
 
     // intermediateOutput0 is the output of the first Add operation.
-    const intermediateOutput0 = nn.add(tensor0, tensor1);
+    const intermediateOutput0 = nn.add(constant1, input1);
 
     // intermediateOutput1 is the output of the second Add operation.
-    const intermediateOutput1 = nn.add(tensor2, tensor3);
+    const intermediateOutput1 = nn.add(constant2, input2);
 
     // output is the output tensor of the Mul operation.
     const output = nn.mul(intermediateOutput0, intermediateOutput1);
 
-    this.model_ = await nn.createModel([output])
+    this.model_ = await nn.createModel([{name: 'output', operand: output}]);
   }
 
   async compile(options) {
@@ -51,19 +51,21 @@ class SimpleModel {
   }
 
   async compute(inputValue1, inputValue2) {
+    // Create an Execution object for the compiled model.
     const execution = await this.compilation_.createExecution();
-    const inputTensor1 = new Float32Array(this.tensorSize_);
-    inputTensor1.fill(inputValue1);
-    const inputTensor2 = new Float32Array(this.tensorSize_);
-    inputTensor2.fill(inputValue2);
 
-    // Tell the execution to associate inputTensor1 to the first of the two model inputs.
-    // Note that the index of the modelInput list {tensor1, tensor3}
-    execution.setInput(0, inputTensor1);
-    execution.setInput(1, inputTensor2);
+    const inputBuffer1 = new Float32Array(this.tensorSize_);
+    inputBuffer1.fill(inputValue1);
+    const inputBuffer2 = new Float32Array(this.tensorSize_);
+    inputBuffer2.fill(inputValue2);
 
+    // Associate the input buffers to model's inputs.
+    execution.setInput('input1', inputBuffer1);
+    execution.setInput('input2', inputBuffer2);
+
+    // Associate the output buffer to model's output.
     const outputTensor = new Float32Array(this.tensorSize_);
-    execution.setOutput(0, outputTensor);
+    execution.setOutput('output', outputTensor);
 
     await execution.startCompute();
 
