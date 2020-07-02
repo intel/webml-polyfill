862fbd34b2314ebce2368cec846bf08384d76c73
Belem Zhang <belem.zhang@intel.com>
2020-07-02 13:46:34 +0800

    [webnnmeeting] update IP address

--- examples/webnnmeeting/config.js
+++ examples/webnnmeeting/config.js
@@ -3,7 +3,7 @@ module.exports = {
     id: '5df9ca6f7415937c7a91d774',
     key:
       'rGtTQokQM/OeG/9oDzK9TtFjd+OOeUmFN2dZl52mvaI4cSj1waduIJB8x21Wa9MaGqtZzV1KTWBvr7heBIgSjQjQyeBWI0RFzCTSyhFtd9jmZ994xE50Gkmb2zxkQYALef8oj8do3gT/cWfOfgq1zPooCkRtbMK1xm44Avduyj4=',
-    url: 'https://10.239.47.52',
+    url: 'https://xxx.xxx.xxx.xxx',
     port: '3000',
     restapiport: '3004'
   },
