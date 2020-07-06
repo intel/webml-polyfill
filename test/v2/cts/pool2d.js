80470ecd18e7f182d0256be3af19cfa41e712f4f
Ningxin Hu <ningxin.hu@intel.com>
2020-07-06 12:41:38 +0800

    [nn_v2] add test for global average pool2d

--- test/v2/cts/pool2d.js
+++ test/v2/cts/pool2d.js
@@ -111,4 +111,34 @@ describe('CTS', function() {
     const expected = [4, 6, 14, 16];
     checkOutput(outputBuffer, expected);
   });
+
+  it('global averagePool2d', async function() {
+    const input = nn.input('input', {type: 'tensor-float32', dimensions: [1, 3, 5, 5]});
+    const output = nn.averagePool2d(input);
+    const model = await nn.createModel([{name: 'output', operand: output}]);
+    const compilation = await model.createCompilation();
+    const execution = await compilation.createExecution();
+    execution.setInput('input', new Float32Array([
+        -1.1289884 ,  0.34016284,  0.497431  ,  2.1915932 ,  0.42038894,
+        -0.18261199, -0.15769927, -0.26465914,  0.03877424,  0.39492005,
+        -0.33410737,  0.74918455, -1.3542547 , -0.0222946 ,  0.7094626 ,
+        -0.09399617,  0.790736  , -0.75826526,  0.27656242,  0.46543223,
+        -1.2342638 ,  1.1549494 ,  0.24823844,  0.75670505, -1.7108902 ,
+        -1.4767597 , -1.4969662 , -0.31936142,  0.5327554 , -0.06070877,
+        0.31212643,  2.2274113 ,  1.2775147 ,  0.59886885, -1.5765078 ,
+        0.18522178,  0.22655599,  0.88869494,  0.38609484, -0.05860576,
+        -0.72732115, -0.0046324 , -1.3593693 , -0.6295078 ,  1.384531  ,
+        0.06825881,  0.19907428,  0.20298219, -0.8399954 ,  1.3583295 ,
+        0.02117888, -1.0636739 , -0.30460566, -0.92678875, -0.09120782,
+        -0.88333017, -0.9641269 ,  0.6065926 , -0.5830042 , -0.81138134,
+        1.3569402 ,  1.2891295 ,  0.2508177 ,  0.20211531,  0.8832168 ,
+        -0.19886094, -0.61088   ,  0.682026  , -0.5253442 ,  1.5022339 ,
+        1.0256356 ,  1.0642492 , -0.4169051 , -0.8740329 ,  1.1494869
+    ]));
+    const outputBuffer = new Float32Array(3);
+    execution.setOutput('output', outputBuffer);
+    await execution.startCompute();
+    const expected = [0.07170041, 0.05194739, 0.07117923];
+    checkOutput(outputBuffer, expected);
+  });
 });
