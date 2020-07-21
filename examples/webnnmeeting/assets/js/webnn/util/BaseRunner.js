31aa4a1f00b2dcef6202b4b2811fe19929f1581a
Belem Zhang <belem.zhang@intel.com>
2020-07-21 15:38:56 +0800

    [webnnmeeting] Fix lack of initialization issue

--- examples/webnnmeeting/assets/js/webnn/util/BaseRunner.js
+++ examples/webnnmeeting/assets/js/webnn/util/BaseRunner.js
@@ -141,7 +141,7 @@ class BaseRunner {
    * @param {!Object<string, *>} modelInfo An object for model info which was configed in modeZoo.js.
    *     See modelInfo details from above '_setModelInfo' method.
    */
-  doInitialization = (modelInfo) => {};
+  doInitialization = (modelInfo) => { };
 
   /**
    * This method is to load model file and relevant resources, such as label file.
@@ -153,6 +153,8 @@ class BaseRunner {
       console.log(`${this._currentModelInfo.modelFile} already loaded.`);
       return;
     }
+
+    this.doInitialization(modelInfo);
 
     let modelPath = this._currentModelInfo.modelFile;
     // for local workload test
@@ -185,7 +187,7 @@ class BaseRunner {
    *       prefer: {string}, // 'fast'|'sustained'|'low'|'ultra_low'
    *     };
    */
-  _doCompile = (options) => {};
+  _doCompile = (options) => { };
 
   /**
    * This method is to check whether model compiled.
@@ -254,12 +256,12 @@ class BaseRunner {
    *     };
    *     or input is a Typed Array object, likes in Speech Recognition example.
    */
-  _getInputTensor = (input) => {};
+  _getInputTensor = (input) => { };
 
   /**
    * This methode is to do inference with input tensor and output tensor will be updated after successfully inferenced.
    */
-  _doInference = () => {};
+  _doInference = () => { };
 
   /**
    * This method is to do inference with an HTML [<img> | <video> | <audio>] element input.
@@ -286,7 +288,7 @@ class BaseRunner {
    *       outputClassScoresTensor: {!TypedArray<number>},
    *     };
    */
-  _getOutputTensor = () => {};
+  _getOutputTensor = () => { };
 
   /**
    * This method is to get inference result including inference time, computed output tensor
