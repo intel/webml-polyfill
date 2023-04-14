1841928b06e000a306190a01c8e372cdd98bae72
BruceDai <feng.dai@intel.com>
2023-04-14 13:13:01 +0800

    Remove webnnmeeting sample

--- examples/webnnmeeting/assets/js/owt/conference/export.js
+++
@@ -1,8 +0,0 @@
-// Copyright (C) <2018> Intel Corporation
-//
-// SPDX-License-Identifier: Apache-2.0
-
-'use strict';
-
-export {ConferenceClient} from './client.js';
-export {SioSignaling} from './signaling.js';
